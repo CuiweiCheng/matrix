9f3ffb65598659c3a57597df77c94dca3eb778b3
asher <yijie.fu@columbia.edu>
2018-11-29 15:35:21 -0500

    upadte MLmodel File

--- MLmodel/NeuralNetwork.py
+++ MLmodel/NeuralNetwork.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn import preprocessing
 
+
 def _init_layers(nn_architecture, seed):
     np.random.seed(seed)
     number_of_layers = len(nn_architecture)
@@ -19,27 +20,39 @@ def _init_layers(nn_architecture, seed):
     return params_values
 
 
-
-#define some activation functions that we will use in the future
+# define some activation functions that we will use in the future
 def _sigmoid(Z):
     return 1/(1+np.exp(-Z))
+
+
 def _relu(Z):
-    return np.maximum(0,Z)
+    return np.maximum(0, Z)
+
+
 def _tanh(Z):
     return (np.exp(Z)-np.exp(-Z)) / (np.exp(Z)+np.exp(-Z))
+
 
 def _sigmoid_backward(dA, Z):
     sig = _sigmoid(Z)
     return dA * sig * (1 - sig)
+
+
 def _relu_backward(dA, Z):
-    dZ = np.array(dA, copy = True)
-    dZ[Z <= 0] = 0;
-    return dZ;
+    dZ = np.array(dA, copy=True)
+    dZ[Z <= 0] = 0
+    return dZ
+
+
 def _tanh_backward(dA, Z):
     mid = (1 - ((np.exp(Z)-np.exp(-Z)) / (np.exp(Z)+np.exp(-Z))) ** 2)
     return dA * mid
+
+
 ''' --------------------End of Activation Function ------------------------------'''
-#suppose we are facing with single layer. Compute WA+b to get next layer Z. 
+# suppose we are facing with single layer. Compute WA+b to get next layer Z.
+
+
 def _single_layer_forward_propagation(A_prev, W_curr, b_curr, activation="tanh"):
     Z_curr = np.dot(W_curr, A_prev) + b_curr
 
@@ -54,7 +67,9 @@ def _single_layer_forward_propagation(A_prev, W_curr, b_curr, activation="tanh")
 
     return activation_func(Z_curr), Z_curr
 
-#compute next layer recursively using forward propagation
+# compute next layer recursively using forward propagation
+
+
 def _full_forward_propagation(X, params_values, nn_architecture):
     memory = {}
     A_curr = X
@@ -66,7 +81,8 @@ def _full_forward_propagation(X, params_values, nn_architecture):
         activ_function_curr = layer["activation"]
         W_curr = params_values["W" + str(layer_idx)]
         b_curr = params_values["b" + str(layer_idx)]
-        A_curr, Z_curr = _single_layer_forward_propagation(A_prev, W_curr, b_curr, activ_function_curr)
+        A_curr, Z_curr = _single_layer_forward_propagation(
+            A_prev, W_curr, b_curr, activ_function_curr)
 #        print(A_curr)
         memory["A" + str(idx)] = A_prev
         memory["Z" + str(layer_idx)] = Z_curr
@@ -74,7 +90,9 @@ def _full_forward_propagation(X, params_values, nn_architecture):
     return A_curr, memory
 
 #[Y_hat,memory] = full_forward_propagation(X, params_values, nn_architecture)
-#compute dA,dZ,dW,db using chain rule for previous layer
+# compute dA,dZ,dW,db using chain rule for previous layer
+
+
 def _single_layer_backward_propagation(dA_curr, W_curr, b_curr, Z_curr, A_prev, activation="tanh"):
     m = A_prev.shape[1]
 
@@ -93,13 +111,15 @@ def _single_layer_backward_propagation(dA_curr, W_curr, b_curr, Z_curr, A_prev, 
     dA_prev = np.dot(W_curr.T, dZ_curr)
 
     return dA_prev, dW_curr, db_curr
-#compute derivative recursively to get descent of parameters
+# compute derivative recursively to get descent of parameters
+
+
 def _full_backward_propagation(Y_hat, Y, memory, params_values, nn_architecture):
     grads_values = {}
     m = Y.shape[1]
     Y = Y.reshape(Y_hat.shape)
 
-    dA_prev = - (np.divide(Y, Y_hat) - np.divide(1 - Y, 1 - Y_hat));
+    dA_prev = - (np.divide(Y, Y_hat) - np.divide(1 - Y, 1 - Y_hat))
 
     for layer_idx_prev, layer in reversed(list(enumerate(nn_architecture))):
         layer_idx_curr = layer_idx_prev + 1
@@ -120,23 +140,27 @@ def _full_backward_propagation(Y_hat, Y, memory, params_values, nn_architecture)
 
     return grads_values
 #grads_values = full_backward_propagation(Y_hat, Y, memory, params_values, nn_architecture)
-#print(params_values)
+# print(params_values)
+
 
 def _update(params_values, grads_values, nn_architecture, learning_rate):
-    for layer_idx in range(1,4):
-        params_values["W" + str(layer_idx)] -= learning_rate * grads_values["dW" + str(layer_idx)]        
-        params_values["b" + str(layer_idx)] -= learning_rate * grads_values["db" + str(layer_idx)]
+    for layer_idx in range(1, 4):
+        params_values["W" + str(layer_idx)] -= learning_rate * \
+            grads_values["dW" + str(layer_idx)]
+        params_values["b" + str(layer_idx)] -= learning_rate * \
+            grads_values["db" + str(layer_idx)]
 
-    return params_values;
+    return params_values
 #update(params_values, grads_values, nn_architecture, learning_rate)
-#print(params_values)
+# print(params_values)
 
 
 def _get_cost_value(Y_hat, Y):
-    
+
     L = sum(sum((Y-Y_hat)**2))
-    
+
     return L
+
 
 def _train(X, Y, nn_architecture, epochs, learning_rate):
     params_values = _init_layers(nn_architecture, 2)
@@ -144,15 +168,18 @@ def _train(X, Y, nn_architecture, epochs, learning_rate):
     accuracy_history = []
 
     for i in range(epochs):
-        Y_hat, cashe = _full_forward_propagation(X, params_values, nn_architecture)
+        Y_hat, cashe = _full_forward_propagation(
+            X, params_values, nn_architecture)
         cost = _get_cost_value(Y_hat, Y)
         cost_history.append(cost)
-        print('i = ',i,'cost = ',cost,'avg_unit_cost',cost/(947*252))
+        print('i = ', i, 'cost = ', cost, 'avg_unit_cost', cost/(947*252))
 #        accuracy = get_accuracy_value(Y_hat, Y)
 #        accuracy_history.append(accuracy)
 
-        grads_values = _full_backward_propagation(Y_hat, Y, cashe, params_values, nn_architecture)
-        params_values = _update(params_values, grads_values, nn_architecture, learning_rate)
+        grads_values = _full_backward_propagation(
+            Y_hat, Y, cashe, params_values, nn_architecture)
+        params_values = _update(
+            params_values, grads_values, nn_architecture, learning_rate)
 #        cost_old = cost
 #        if (np.abs(cost_old-cost/cost_old)) < 1e-7:
 #            break
@@ -160,20 +187,23 @@ def _train(X, Y, nn_architecture, epochs, learning_rate):
 
 
 ''' Main Function --- Training '''
-def NNPredict(n,m,t, nn_architecture, epochs, learning_rate,stockdata):
+
+
+def NNPredict(n, m, t, nn_architecture, epochs, learning_rate, stockdata):
     seed = 4500
     total_data = stockdata.values
-    X = total_data[0:n,0:t]
-    Y = total_data[0:n,10:t+10]
-    X = np.reshape(X,[n,t])
+    X = total_data[0:n, 0:t]
+    Y = total_data[0:n, 10:t+10]
+    X = np.reshape(X, [n, t])
     params_values = _init_layers(nn_architecture, seed)
     _train(X, Y, nn_architecture, epochs, learning_rate)
-    #print(params_values['W1'],params_values['W2'],params_values['W3'],params_values['b1'],params_values['b2'],params_values['b3'])
+    # print(params_values['W1'],params_values['W2'],params_values['W3'],params_values['b1'],params_values['b2'],params_values['b3'])
     '''Testing'''
 
-    X = total_data[0:n,253:494]
-    Y = total_data[0:n,253:494]
-    [Y_hat,memory] = _full_forward_propagation(X, params_values, nn_architecture)
-    return_val=_get_cost_value(Y_hat, Y)
-    print('Y_hat=',Y_hat,'Loss = ',return_val)
-    return Y_hat,return_val
+    X = total_data[0:n, 253:494]
+    Y = total_data[0:n, 253:494]
+    [Y_hat, memory] = _full_forward_propagation(
+        X, params_values, nn_architecture)
+    return_val = _get_cost_value(Y_hat, Y)
+    print('Y_hat=', Y_hat, 'Loss = ', return_val)
+    return Y_hat, return_val
