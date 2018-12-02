fc03edda41f67475a114616d5f2ee5f94bb7a87d
CuiweiCheng <cc4309@columbia.edu>
2018-12-01 23:21:47 -0500

    add 2 more func

--- test.py
+++ test.py
@@ -1,10 +1,17 @@
 import pandas as pd
 import stock_price as sp
 
+sp.download_stock_price('AAPL')
 
+x = sp.dataframe_of_single_stock('TSLA')
+print(x)
+
+y = sp.dataframe_of_stocks(['BIDU', 'SINA'])
+print(y)
+
+# for teammates:
+# if you guys want to create a dataframe with lots of stocks, see this.
 df = pd.read_csv('company_list.csv')
-# print(df[:3])
-company_slice = pd.DataFrame(df, index=range(3), columns=['Symbol', 'Name', 'Sector'])
-print(company_slice)
-for i in df['Symbol'][:3]:
-    sp.download_stock_price(i)
+list_of_stock_symbol = df['Symbol'][10:160]  # first twenty stocks in the company list provided.
+z = sp.dataframe_of_stocks(list_of_stock_symbol)
+print(z)
